62514f04422983fa8277e82ddb7e5a03984986de
rizafrivaldi <rizafrivaldi@gmail.com>
2025-11-09 22:34:22 +0700

    new update

--- express-middleware-demo/middleware/logger.js
+++
@@ -1,6 +0,0 @@
-const logger = (req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-  next(); //cont for the next middleware//
-};
-
-module.exports = logger;
--- express-middleware-demo/utils/customError.js
+++
@@ -1,8 +0,0 @@
-class customError extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-  }
-}
-
-module.exports = customError;
